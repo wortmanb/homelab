cdeb1610d7c554cf172add1c8ad793400a2b1d75
Bret Wortman <bret.wortman@damascusgrp.com>
2024-11-03 08:24:15 -0600

    Moved vault code
    Took this out of main.tf and moved it to vault.tf.

---
+++ tf/vault.tf
@@ -0,0 +1,20 @@
+# vault.tf
+
+# Retrieve PM_USER from Vault
+data "vault_generic_secret" "pm_user" {
+  path = "secret/pm_user"
+}
+
+# Retrieve PM_PASS from Vault
+data "vault_generic_secret" "pm_pass" {
+  path = "secret/pm_pass"
+}
+
+# Use the secrets in a resource, output, or variable
+output "pm_user" {
+  value = data.vault_generic_secret.pm_user.data["data"]["PM_USER"]
+}
+
+output "pm_pass" {
+  value = data.vault_generic_secret.pm_pass.data["data"]["PM_PASS"]
+}
