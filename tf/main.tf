b42c857d3aad49130186c428a8c3e1d402d9fb3a
Bret Wortman <bret.wortman@damascusgrp.com>
2024-11-03 08:49:49 -0600

    Refactoring to add a proxmox-vm module

--- tf/main.tf
+++ tf/main.tf
@@ -1,26 +1,22 @@
 # main.terraform {
 # Define the various VMs we need.
 
-resource "proxmox_vm_qemu" "linux_vm" {
-    name        = var.name
-    target_node = var.target_node
-    vmid        = var.vmid
-    clone       = var.clone
+# Iterate over each VM in the list and create a module instance
+module "proxmox_vms" {
+  source = "./modules/proxmox_vm"
+  for_each = var.vms
 
-    os_type = var.os_type
-    cores   = var.cores
-    memory  = var.memory
-    sockets = var.sockets
-
-    disk {
-        size    = var.disk_size
-        storage = var.disk_storage
-    }
-
-    network {
-        model  = var.network_model
-        bridge = var.network_bridge
-    }
-
-    ipconfig0 = var.ipconfig0
+  name           = each.value.name
+  target_node    = each.value.target_node
+  vmid           = each.value.vmid
+  clone          = each.value.clone
+  os_type        = each.value.os_type
+  cores          = each.value.cores
+  memory         = each.value.memory
+  sockets        = each.value.sockets
+  disk_size      = each.value.disk_size
+  disk_storage   = each.value.disk_storage
+  network_model  = each.value.network_model
+  network_bridge = each.value.network_bridge
+  ipconfig0      = each.value.ipconfig0
 }
